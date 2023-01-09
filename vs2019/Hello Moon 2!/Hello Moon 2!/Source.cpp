5617c042fc1d9f66d4443fedb74b14addb63223d
MarioXHK <104402858+MarioXHK@users.noreply.github.com>
2023-01-09 14:47:34 -0700

    CODE!!!
    YES!!!!!!!!!!!!!!!!

---
+++ vs2019/Hello Moon 2!/Hello Moon 2!/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+//Making sure the function exists
+void printHello();
+
+int main() {
+    cout << "Hello, Moon!" << endl;
+    cout << "This is the story of a man named Stanley." << endl;
+    cout << "Stanley worked for a company in a big building where he was Employee #427." << endl;
+    cout << "Employee #427's job was simple: he sat at his desk in Room 427 and he pushed buttons on a keyboard." << endl;
+    cout << "Orders came to him through a monitor on his desk telling him what buttons to push, how long to push them, and in what order." << endl;
+    cout << "This is what Employee #427 did every day of every month of every year, and although others may have considered it soul rending," << endl;
+    cout << "Stanley relished every moment that the orders came in, as though he had been made exactly for this job." << endl;
+    cout << "And Stanley was happy." << endl;
+    cout << "And then one day, something very peculiar happened." << endl;
+    cout << "Something that would forever change Stanley;" << endl;
+    cout << "Something he would never quite forget." << endl;
+    cout << "He had been at his desk for nearly an hour when he had realized not one single order had arrived on the monitor for him to follow." << endl;
+    cout << "No one had shown up to give him instructions, call a meeting, or even say 'hi'." << endl;
+    cout << "Never in all his years at the company had this happened, this complete isolation." << endl;
+    cout << "Something was very clearly wrong. Shocked, frozen solid, Stanley found himself unable to move for the longest time." << endl;
+    cout << "But as he came to his wits and regained his senses, he got up from his desk and stepped out of his office." << endl;
+    printHello();
+}
+
+void printHello() {
+    cout << "Hello!!!" << endl;
+    cout << "Welcome to [Hyperlink Blocked] the [REDACTED] of the entire [Data expunged]!" << endl;
+    cout << "Use the Joystick to move around. Pointing up on the Sadstick will make you jump." << endl;
+    cout << "Press on the Ragestick to fire your omega laser. Press on the Anguishstick to pause the game." << endl;
+}
---
+++ vs2019/TheseLoops/TheseLoops/Source.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+void Snowflake(int stuff);
+
+int main() {
+    cout << "Hello, Loops!" << endl;
+    for (int i = 0; i < 10; i++)
+        cout << i << " ";
+    cout << endl;
+    for (int j = 5; j <= 30; j += 5)
+        cout << j << " ";
+    cout << endl;
+    for (int k = 100; k >= 0; k -= 10)
+        cout << k << " ";
+    cout << endl;
+    for (int l = 2; l < 40; l *= 2)
+        cout << l << " ";
+    cout << endl;
+    for (int m = 70; m <= 80; m++)
+        cout << m << " ";
+    cout << endl;
+    for (int n = 100; n <= 300; n += 20)
+        cout << n << " ";
+    cout << endl;
+    for (int o = 25; o >= -25; o -= 5)
+        cout << o << " ";
+    cout << endl;
+    for (int p = 10000; p >= 10; p /= 5)
+        cout << p << " ";
+    cout << endl;
+    
+    for (int q = 0; q < 10; q++) {
+        cout << endl;
+        Snowflake(q);
+        cout << endl;
+    }
+    cout << "Goodbye, Loops!" << endl;
+}
+
+void Snowflake(int stuff) {
+    int eb = stuff * 2;
+    for (int h = eb; h >= 0; h -= 2) {
+        for (int j = h/2; j < eb-1; j++)
+            cout << " ";
+        cout << "_\\/";
+        for (int i = 0; i < h; i++)
+            cout << " ";
+        cout << "\\/_" << endl;
+    }
+    for (int i = 0; i <= stuff; i++)
+        cout << "_\\";
+    for (int i = 0; i <= stuff; i++)
+        cout << "/_";
+    cout << endl;
+    for (int i = 0; i <= stuff; i++)
+        cout << " /";
+    for (int i = 0; i <= stuff; i++)
+        cout << "\\ ";
+    cout << endl;
+    for (int k = 0; k <= eb; k += 2) {
+        for (int j = k / 2; j < eb - 1; j++)
+            cout << " ";
+        cout << "_/\\";
+        for (int i = 0; i < k; i++)
+            cout << " ";
+        cout << "/\\_" << endl;
+    }
+}
