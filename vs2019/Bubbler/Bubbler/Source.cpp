9cd5b113352c49ffdf5d5df212d82aea06e11ac7
MarioXHK <104402858+MarioXHK@users.noreply.github.com>
2023-01-10 14:51:46 -0700

    Oh no, our table, it's broken.
    WHAT IS HAPPENIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII-

---
+++ vs2019/Bubbler/Bubbler/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void bubbler(int lelist);
+
+int main() {
+    cout << "Hello, Bubbles!" << endl;
+    int thisthing[] = { 6,7,1,2,5 };
+    for (int i = 0; i < sizeof(thisthing); i++) {
+        cout << thisthing[i] << " ";
+    }
+    cout << endl << "Goodbye, Loops!" << endl;
+}
+//The function of how to do snowflakes!
+void bubbler(int lelist) {
+    
+}
