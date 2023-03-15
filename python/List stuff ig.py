28273745a1819502f6c2474eeb37d7d9b22eb31a
MarioXHK <104402858+MarioXHK@users.noreply.github.com>
2023-03-15 14:19:51 -0600

    23 lines
    only 23 lines????

--- python/List stuff ig.py
+++ python/List stuff ig.py
@@ -1,3 +1,4 @@
+#A program that's list practice. Personal challenge: Keep the program equal to 23 lines.
 import random
 def conse(lis):
     lis.sort()
@@ -10,14 +11,13 @@ def conse(lis):
         if counter >= 3:
             return True
     return False
-last = []
-for i in range(12):
-    last.append(random.randint(1,10))
-print(last)
-last.sort()
-print(last)
-print(max(last))
-print("The list does", end = " ")
-if not conse(last):
-     print("not", end = " ")
-print("have at least three consecutive values in it")
+def reverse(lis):
+    las = []
+    for i in range(len(lis)):
+        las.append(lis[len(lis)-i-1])
+    return las
+for i in range(5):
+    last = [random.randint(1,10),random.randint(1,10),random.randint(1,10),random.randint(1,10),random.randint(1,10),random.randint(1,10),random.randint(1,10),random.randint(1,10),random.randint(1,10),random.randint(1,10),random.randint(1,10),random.randint(1,10)]
+    print(last,"\nMax of the list is", max(last),"\nIt is", conse(last), "that the list has at least three consecutive values in it.\nThe list reversed is", reverse(last))
+else:
+    print("ez")
